158a0e3dba1cc4a6553c597ff674a9ce5fa84c8a
Simon Ser <contact@emersion.fr>
2019-05-07 23:38:45 +0300

    Add Connector consts

--- const.go
+++ const.go
@@ -8,3 +8,27 @@ const (
 	CapDumbPreferredDepth Cap = 0x3
 	CapDumbPreferredShadow Cap = 0x4
 )
+
+type Connector uint32
+
+const (
+	ConnectorUnknown Connector = 0
+	ConnectorVGA Connector = 1
+	ConnectorDVII Connector = 2
+	ConnectorDVID Connector = 3
+	ConnectorDVIA Connector = 4
+	ConnectorComposite Connector = 5
+	ConnectorSVideo Connector = 6
+	ConnectorLVDS Connector = 7
+	ConnectorComponent Connector = 8
+	Connector9PinDIN Connector = 9
+	ConnectorDisplayPort Connector = 10
+	ConnectorHDMIA Connector = 11
+	ConnectorHDMIB Connector = 12
+	ConnectorTV Connector = 13
+	ConnectorEDP Connector = 14
+	ConnectorVirtual Connector = 15
+	ConnectorDSI Connector = 16
+	ConnectorDPI Connector = 17
+	ConnectorWriteback Connector = 18
+)
